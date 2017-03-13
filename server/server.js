d79c6391a9b02701d846f83f38e7b563399662bc
James Mitchell <jamesmitchell@Jamess-MacBook-Air.local>
2017-03-13 18:40:21 -0500

    modified server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var app = express()
+var app = express();
 
 mongoose.connect('mongodb://localhost/shortly');
 
 app.get('/', function (req, res) {
-  res.send('index.html')
+  res.send(__dirname + '/client/index.html')
 })
 
-app.listen(27017, function () {
-  console.log('PlayAlong app listening on port 27017!')
+app.listen(process.env.PORT || 3000, function () {
+  console.log('PlayAlong app listening on port 3000!')
 })
