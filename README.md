02129bb1148a5c410f9c65f920fa075f44fdebe3
James Mitchell <jamesmitchell@Jamess-MacBook-Air.local>
2017-03-13 15:11:15 -0500

    deploy app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PlayAlong
+
+deployed app: https://git.heroku.com/fast-chamber-35495.git
--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
-var express = require('express')
+var express = require('express');
+var mongoose = require('mongoose');
 var app = express()
+
+mongoose.connect('mongodb://localhost/shortly');
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('PlayAlong app listening on port 3000!')
+app.listen(27017, function () {
+  console.log('PlayAlong app listening on port 27017!')
 })
